2c86593721bda655e5f6594d55f5e3ea4fbe3c81
Nilesh kadam <nilesh.kadam222@gmail.com>
2021-04-03 20:23:00 +0530

    @Configuration and @PropertySource

--- spring-demo-annotation/src/com/myApp/SportConfiguration.java
+++ spring-demo-annotation/src/com/myApp/SportConfiguration.java
@@ -3,9 +3,11 @@ package com.myApp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.myApp")
+@PropertySource("classpath:application.properties")
 public class SportConfiguration {
 
 	//createing Fortune service bean
--- spring-demo-annotation/src/com/myApp/SwmiCoach.java
+++ spring-demo-annotation/src/com/myApp/SwmiCoach.java
@@ -2,8 +2,17 @@ package com.myApp;
 
 import java.util.Iterator;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwmiCoach implements Coach {
 
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.name}")
+	private String name;
+	
+	
 	private FortuneService fortuneService;
 	
 	public SwmiCoach(FortuneService theFortuneService) {
@@ -12,6 +21,8 @@ public class SwmiCoach implements Coach {
 	
 	@Override
 	public String getDailyWorkout() {
+		System.out.println(email);
+		System.out.println(name);
 		return "Swmi 1000 meter daily";
 	}
 
